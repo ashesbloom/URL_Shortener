6371e455435d8eeb97c20b3c461fdea7e30c76c1
ashesbloom <mayankpandeydk123@gmail.com>
2024-06-30 17:10:20 +0530

    minor bug fixes documented views and javascript files

--- public/javascripts/copyText.js
+++ public/javascripts/copyText.js
@@ -1,8 +1,8 @@
 // Get the elements
-let text = document.getElementById("copy_text");
-let copiedText = text.textContent;
-let copyButton = document.getElementById("copy");
-let copySuccess = document.querySelector(".msg");
+let text = document.getElementById("copy_text"); //the object to be copied
+let copiedText = text.textContent; //extracting the text from the object
+let copyButton = document.getElementById("copy"); //the copy icon
+let copySuccess = document.querySelector(".msg"); //the message to be displayed after copying
 
 // Add event listener to the copy icon
 copyButton.addEventListener("click", function() {
@@ -18,16 +18,17 @@ copyButton.addEventListener("click", function() {
     // For mobile devices
     tempTextArea.setSelectionRange(0, 99999);
 
-    // Execute the copy command
-    document.execCommand('copy');
+    // Use the Clipboard API to copy the text
+    navigator.clipboard.writeText(tempTextArea.value).then(function(){  //we could also use document.execCommand('copy') but it is deprecated
+        // Remove the temporary textarea
+        document.body.removeChild(tempTextArea);
 
-    // Remove the temporary textarea
-    document.body.removeChild(tempTextArea);
-
-    // Display the copy message
-    copySuccess.style.display = 'inline-block';
-    setTimeout(function(){
-        copySuccess.style.display = "none";
-    },1500);
-    
+        // Display the copy message
+        copySuccess.style.display = 'inline-block';
+        setTimeout(function(){
+            copySuccess.style.display = "none";
+        },1500);
+    }).catch(err =>{
+        console.error('Failed to copy: ',err);
+    })
 });
--- public/javascripts/handleAction.js
+++ public/javascripts/handleAction.js
@@ -1,18 +1,18 @@
-async function handleLogout(url) {
+async function handleLogout(url) { //handling the logout request form the user
     try {
-        const response = await fetch(url, { method: 'post' });
-        const result = await response.json();
-        if (result.redirect) {
-            window.location.href = result.redirect;
+        const response = await fetch(url, { method: 'post' }); //fetching the url with post method
+        const result = await response.json(); //converting the response into json
+        if (result.redirect) {//if the result has redirect key then redirect to the url
+            window.location.href = result.redirect; 
         }
-    } catch (error) {
+    } catch (error) {//if any error occurs then log the error
         console.error({ error });
     }
 }
 
-async function handle_ClearData(url) {
+async function handle_ClearData(url) { //handling the clear data request from the user
     try {
-        const confirmed = await swal({
+        const confirmed = await swal({ //using sweetalert to show the confirmation dialog (https://www.npmjs.com/package/sweetalert)
             title: 'Are you sure?',
             text: 'This will clear all the records!',
             icon: 'info',
@@ -20,44 +20,46 @@ async function handle_ClearData(url) {
             dangerMode: true,
         });
 
-        if (confirmed) {
-            const response = await fetch(url, { method: 'post' });
-            const result = await response.json();
-            if (result.redirect) {
+        if (confirmed) { //if the user confirms the dialog then fetch the url with post method
+            const response = await fetch(url, { method: 'post' }); //fetching the url with post method
+            const result = await response.json(); //converting the response into json
+            if (result.redirect) { //if the result has redirect key then redirect to the url
                 window.location.href = result.redirect;
             }
         }
-    } catch (error) {
+    } catch (error) { //if any error occurs then log the error
         console.error({ error });
     }
 }
 
-async function handle_Userdelete(url,email){
+async function handle_Userdelete(url,email){ //handling the user delete request from the admin
     try{
-        const confirmed = await swal({
+        const confirmed = await swal({ //using sweetalert to show the confirmation dialog
                 title: 'Deleting this user?',
                 text: 'User\'s urls will also be cleared!',
                 icon: 'info',
                 buttons: true,
                 dangerMode: true,
         });
-        if (confirmed) {
-            const response = await fetch(url, { 
+        if (confirmed) { //if the user confirms the dialog then fetch the url with post method
+            const response = await fetch(url, { //fetching the url with post method
                 method: 'post',
-                headers: {
+                headers: {  //passing the headers to request the user from database
                     'email':email
                 }
              });
-            const result = await response.json();
-            if (result.redirect) {
+            const result = await response.json(); //converting the response into json
+            if (result.redirect) { //if the result has redirect key then redirect to the url
                 window.location.href = result.redirect;
             }
         }
-    } catch (error) {
+    } catch (error) { //if any error occurs then log the error
         console.error({ error });
     }
 }
+
+// Event listeners
 document.addEventListener('DOMContentLoaded', () => {
-    document.getElementById('logout').addEventListener('click', () => handleLogout('/user/logout'));
-    document.getElementById('clear').addEventListener('click', () => handle_ClearData('/delete'));
+    document.getElementById('logout').addEventListener('click', () => handleLogout('/user/logout')); //adding event listener to the logout button
+    document.getElementById('clear').addEventListener('click', () => handle_ClearData('/delete')); //adding event listener to the clear data button
 });
